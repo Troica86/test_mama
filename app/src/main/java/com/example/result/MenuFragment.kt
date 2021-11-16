49e1324df8e2d888fc33645f5d047e7b0adc3f57
troica86 <sgalya2006@yandex.ru>
2021-11-16 21:24:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/result/MenuFragment.kt
@@ -0,0 +1,35 @@
+package com.example.result
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class MenuFragment : Fragment() {
+
+    private lateinit var aboutButton: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_menu, container, false)
+        aboutButton = view.findViewById(R.id.about_button)
+
+        val aboutFragment = infoFragment()
+        aboutButton.setOnClickListener {
+            activity?.supportFragmentManager
+                ?.beginTransaction()
+                ?.replace(R.id.fragment_container, aboutFragment)
+                ?.commit()
+        }
+
+
+
+        return view
+    }
+
+
+}
