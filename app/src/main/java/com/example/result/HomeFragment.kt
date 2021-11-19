d44c17ee0d81d38ac282471a4ef975c5a02e1fdb
troica86 <sgalya2006@yandex.ru>
2021-11-19 09:08:10 +0300

    9

--- app/src/main/java/com/example/result/HomeFragment.kt
+++ app/src/main/java/com/example/result/HomeFragment.kt
@@ -1,35 +1,37 @@
 package com.example.result
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.example.result.TestFragment as TestFragment
 
-class MenuFragment : Fragment() {
+class HomeFragment : Fragment() {
 
-    private lateinit var aboutButton: Button
+    private lateinit var testButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_menu, container, false)
-        aboutButton = view.findViewById(R.id.about_button)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        testButton = view.findViewById(R.id.button_start)
 
-        val aboutFragment = infoFragment()
-        aboutButton.setOnClickListener {
+        val testFragment = TestFragment()
+        testButton.setOnClickListener {
             activity?.supportFragmentManager
                 ?.beginTransaction()
-                ?.replace(R.id.fragment_container, aboutFragment)
+                ?.replace(R.id.fragment_container, testFragment)
                 ?.commit()
         }
 
-
-
         return view
     }
+
 
 
 }
